52fc2a15627b8ec4cda2990cfc0c359fb229c159
dhorri <dhorri@mecon.ar>
2014-03-27 17:30:54 -0300

    test for LoginServiceImpl

--- TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
+++ TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
@@ -1,5 +1,8 @@
 package ar.gov.mecon.mocks;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
 /**
  * http://schuchert.wikispaces.com/Mockito.LoginServiceExample
  * 
@@ -7,4 +10,20 @@ package ar.gov.mecon.mocks;
  */
 public class LoginServiceImplTest {
 
+  @Test
+  public void testName() throws Exception {
+    // given
+    IUsuario usuario = Mockito.mock(IUsuario.class);
+    Mockito.when(usuario.passwordMatches(Mockito.anyString())).thenReturn(true);
+    IRepository repository = Mockito.mock(IRepository.class);
+    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+
+    LoginServiceImpl service = new LoginServiceImpl(repository);
+
+    service.login("dan", "password");
+
+    // verifico que el metodo setLogueado(true) se haya llamado una vez
+    Mockito.verify(usuario, Mockito.times(1)).setLogueado(true);
+
+  }
 }
