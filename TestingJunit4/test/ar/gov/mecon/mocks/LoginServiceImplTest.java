b13bf06120002651f61af3b0a8fed40a43aab2b4
dhorri <dhorri@mecon.ar>
2014-03-28 10:38:27 -0300

    refactor method setearRevocadoCuandoFallaLogin3veces

--- TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
+++ TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
@@ -21,30 +21,30 @@ public class LoginServiceImplTest {
   public void init() {
     usuario = Mockito.mock(IUsuario.class);
     repository = Mockito.mock(IRepository.class);
+    // inyeccion de dependencias
     service = new LoginServiceImpl(repository);
   }
 
   @Test
   public void setearLogueadoEnTrueCuandoPasswordCoincide() {
-    Mockito.when(usuario.passwordMatches(Mockito.anyString())).thenReturn(true);
+    pasaPassword(usuario, true);
     Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
-
-    // inyeccion de dependencias
-
     // when
     service.login("dan", "password");
-
     // verifico que el metodo setLogueado(true) se haya llamado una vez
     Mockito.verify(usuario, Mockito.times(1)).setLogueado(true);
 
   }
 
+  private void pasaPassword(IUsuario usuario, boolean pasa) {
+    Mockito.when(usuario.passwordMatches(Mockito.anyString())).thenReturn(pasa);
+  }
+
   @Test
   public void setearRevocadoCuandoFallaLogin3veces() throws Exception {
-    Mockito.when(usuario.passwordMatches(Mockito.anyString())).thenReturn(false);
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    pasaPassword(usuario, false);
 
-    LoginServiceImpl service = new LoginServiceImpl(repository);
+    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
 
     // when
     for (int i = 0; i < 3; i++) {
@@ -59,19 +59,35 @@ public class LoginServiceImplTest {
   @Test
   public void noSetearLogueadoTrueSiLoginFalla() throws Exception {
     // given
-    Mockito.when(usuario.passwordMatches(Mockito.anyString())).thenReturn(false);
+    pasaPassword(usuario, false);
     Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
 
     // when
-    LoginServiceImpl service = new LoginServiceImpl(repository);
-
     for (int i = 0; i < 3; i++) {
       service.login("dan", "password");
     }
-
     // then
     Mockito.verify(usuario, Mockito.never()).setLogueado(true);
-    ;
+  }
+
+  @Test
+  public void noSetearRevocadoAUnUsuarioEnElPrimerIntento() throws Exception {
+    // given
+    IUsuario otroUsuario = Mockito.mock(IUsuario.class);
+    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    Mockito.when(repository.find("Daniel")).thenReturn(otroUsuario);
+
+    pasaPassword(usuario, false);
+    pasaPassword(otroUsuario, true);
+
+    // when
+    service.login("dan", "password");
+    service.login("dan", "password");
+    service.login("Daniel", "password");
+
+    // then
+    Mockito.verify(otroUsuario, Mockito.never()).setRechazado(true);
 
   }
+
 }
