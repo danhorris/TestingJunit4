9b4dbf37142f44fbb909120740da65a7d11937b9
dhorri <dhorri@mecon.ar>
2014-04-01 12:20:57 -0300

    add suite for Usuario and Login

---
+++ TestingJunit4/test/ar/gov/mecon/user/UsuarioLoginSuite.java
@@ -0,0 +1,13 @@
+package ar.gov.mecon.user;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+    UsuarioTest.class, LoginServiceImplTest.class
+})
+public class UsuarioLoginSuite {
+
+}
