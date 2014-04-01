f4c4664a5b5c0c45f0fde51fb7d32ec1a8a3eb8f
dhorri <dhorri@mecon.ar>
2014-04-01 10:32:45 -0300

    rename variables and fix mock of authRepository

--- TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
+++ TestingJunit4/test/ar/gov/mecon/mocks/LoginServiceImplTest.java
@@ -2,6 +2,7 @@ package ar.gov.mecon.mocks;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import ar.gov.mecon.mocks.exceptions.UsuarioLogueadoException;
@@ -17,38 +18,60 @@ public class LoginServiceImplTest {
 
   private IUsuario usuario;
 
-  private IRepository repository;
+  private IRepository userRepository;
+
+  private IAuthRepository repositoryLDAP;
 
   private LoginServiceImpl service;
 
+  /**
+   * 
+   */
   @Before
   public void init() {
     usuario = Mockito.mock(IUsuario.class);
-    repository = Mockito.mock(IRepository.class);
+    Mockito.when(usuario.getLogin()).thenReturn("dan");
+
+    userRepository = Mockito.mock(IRepository.class);
+    repositoryLDAP = Mockito.mock(IAuthRepository.class);
+
     // inyeccion de dependencias
-    service = new LoginServiceImpl(repository);
+    service = new LoginServiceImpl(userRepository, repositoryLDAP);
   }
 
-  private void pasaPassword(IUsuario usuario, boolean pasa) {
-    Mockito.when(usuario.passwordCorrecta(Mockito.anyString())).thenReturn(pasa);
+  /**
+   * If you are using argument matchers, all arguments have to be provided by matchers. {@link Matchers}
+   * 
+   * @param loginUsuario
+   * @param loginCorrecto
+   */
+  private void pasaPasswordPorLdap(String loginUsuario, boolean loginCorrecto) {
+    Mockito.when(repositoryLDAP.verificarPassWord(Mockito.eq(loginUsuario), Mockito.anyString())).thenReturn(
+        loginCorrecto);
   }
 
+  /**
+   * 
+   */
   @Test
   public void logueadoEnTrueCuandoPasswordCoincide() {
-    pasaPassword(usuario, true);
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    pasaPasswordPorLdap("dan", true);
+    Mockito.when(userRepository.find(Mockito.anyString())).thenReturn(usuario);
     // when
-    service.login("dan", "password");
+    service.login("dan", "");
     // verifico que el metodo setLogueado(true) se haya llamado una vez
     Mockito.verify(usuario, Mockito.times(1)).setLogueado(true);
 
   }
 
+  /**
+   * 
+   */
   @Test
-  public void revocadoCuandoFallaLogin3veces() throws Exception {
-    pasaPassword(usuario, false);
+  public void revocadoCuandoFallaLogin3veces() {
+    pasaPasswordPorLdap("dan", false);
 
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    Mockito.when(userRepository.find(Mockito.anyString())).thenReturn(usuario);
 
     // when
     for (int i = 0; i < 3; i++) {
@@ -60,11 +83,14 @@ public class LoginServiceImplTest {
 
   }
 
+  /**
+   * 
+   */
   @Test
-  public void noSetearLogueadoTrueSiLoginFalla() throws Exception {
+  public void noSetearLogueadoTrueSiLoginFalla() {
     // given
-    pasaPassword(usuario, false);
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    pasaPasswordPorLdap("dan", false);
+    Mockito.when(userRepository.find(Mockito.anyString())).thenReturn(usuario);
 
     // when
     for (int i = 0; i < 3; i++) {
@@ -74,15 +100,18 @@ public class LoginServiceImplTest {
     Mockito.verify(usuario, Mockito.never()).setLogueado(true);
   }
 
+  /**
+   * 
+   */
   @Test
-  public void noSetearRevocadoAUnUsuarioEnElPrimerIntento() throws Exception {
+  public void noSetearRevocadoAUnUsuarioEnElPrimerIntento() {
     // given
     IUsuario segundoUsuario = Mockito.mock(IUsuario.class);
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
-    Mockito.when(repository.find("Daniel")).thenReturn(segundoUsuario);
+    Mockito.when(userRepository.find(Mockito.anyString())).thenReturn(usuario);
+    Mockito.when(userRepository.find("Daniel")).thenReturn(segundoUsuario);
 
-    pasaPassword(usuario, false);
-    pasaPassword(segundoUsuario, false);
+    pasaPasswordPorLdap("dan", false);
+    pasaPasswordPorLdap("Daniel", false);
 
     // when
     service.login("dan", "password");
@@ -94,31 +123,41 @@ public class LoginServiceImplTest {
 
   }
 
+  /**
+   * 
+   */
   @Test(expected = UsuarioLogueadoException.class)
   public void noPermitirSegundoLogin() {
     // given
-    pasaPassword(usuario, true);
-    Mockito.when(repository.find(Mockito.anyString())).thenReturn(usuario);
+    pasaPasswordPorLdap("dan", true);
+
+    Mockito.when(userRepository.find(Mockito.anyString())).thenReturn(usuario);
     Mockito.when(usuario.isLogueado()).thenReturn(true);
     // when
     service.login("dan", "password");
 
   }
 
+  /**
+   * 
+   */
   @Test(expected = UsuarioNoExisteException.class)
   public void usuarioNoExisteException() {
     // given
-    Mockito.when(repository.find("dan")).thenReturn(null);
+    Mockito.when(userRepository.find("dan")).thenReturn(null);
     // when
     service.login("dan", "password");
     // then
 
   }
 
+  /**
+   * 
+   */
   @Test(expected = UsuarioRechazadoException.class)
   public void NoLoguearAunUsuarioRechazado() {
     // given
-    Mockito.when(repository.find("dan")).thenReturn(usuario);
+    Mockito.when(userRepository.find("dan")).thenReturn(usuario);
     Mockito.when(usuario.isRechazado()).thenReturn(true);
     // when
     service.login("dan", "password");
