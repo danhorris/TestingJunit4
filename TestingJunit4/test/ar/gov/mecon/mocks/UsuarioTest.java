d066016691ada2a5ed7503e099305dcb20a8378c
dhorri <dhorri@mecon.ar>
2014-04-01 12:14:27 -0300

    add class test for Usuario

---
+++ TestingJunit4/test/ar/gov/mecon/mocks/UsuarioTest.java
@@ -0,0 +1,38 @@
+package ar.gov.mecon.mocks;
+
+import org.junit.Test;
+
+import ar.gov.mecon.mocks.exceptions.UsuarioLogueadoException;
+import ar.gov.mecon.mocks.exceptions.UsuarioRechazadoException;
+
+public class UsuarioTest {
+
+  /**
+   * 
+   */
+  @Test(expected = UsuarioLogueadoException.class)
+  public void noPermitirSegundoLogin() {
+    // given
+    Usuario usuario = new Usuario();
+    usuario.setLogueado(true);
+
+    // when and then
+    usuario.login();
+
+  }
+
+  /**
+   * 
+   */
+  @Test(expected = UsuarioRechazadoException.class)
+  public void noLoguearAunUsuarioRechazado() {
+    // given
+    Usuario usuario = new Usuario();
+    usuario.setRechazado(true);
+
+    // when and then
+    usuario.login();
+
+  }
+
+}
